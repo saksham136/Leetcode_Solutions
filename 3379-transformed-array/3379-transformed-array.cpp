d006295310ea48680d7b66be3bc235be979c4e5f
Saksham Singh <138057288+saksham136@users.noreply.github.com>
2026-02-05 21:28:11 +0530

    Time: 6 ms (58.77%), Space: 25.8 MB (39.55%) - LeetHub

---
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<int> constructTransformedArray(vector<int>& nums) {
+
+        int n = nums.size();
+        vector<int> result(n);
+
+        for(int i = 0; i < n; i++) {
+
+            int j = ((i + nums[i]) % n + n) % n;
+
+            result[i] = nums[j];
+        }
+
+        return result;
+    }
+};
